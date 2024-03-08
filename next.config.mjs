59275729d4798a6b36aaccc37a7e9d9ae3eb3171
Miguel Rodríguez González <uo263494@uniovi.es>
2024-03-08 20:06:28 +0100

    update config again

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: "/mi-marcador-padel",
     output: "export",
+    reactStrictMode: true,
+
 };
 
 export default nextConfig;
